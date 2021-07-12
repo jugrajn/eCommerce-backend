c5194f24897c8c4f554bef8bab7573a9023b31d8
jugrajn <jugraj.nijjar@gmail.com>
2021-07-12 16:01:19 -0400

    updated server and fixed index.js models

--- models/index.js
+++ models/index.js
@@ -11,20 +11,23 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 })
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
-    model: ProductTag
+    model: ProductTag,
+    foreignKey: 'product_id'
   }
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
-    model: ProductTag
+    model: ProductTag,
+    foreignKey: 'product_id'
   }
 })
 
